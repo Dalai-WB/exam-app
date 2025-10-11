ac7ba37dc2fe840915076b6d5aea8f6dbc2ddc06
Dalai <edalai0115@gmail.com>
2025-10-11 17:51:51 +0800

    [fix] fix

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -9,35 +9,28 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
   profileForm!: FormGroup;
-  profilePicture: string = 'assets/images/Unknown_person.jpg'; // placeholder image
   student: any;
+  studentStats: any;
+  top10: any[] = [];
 
   constructor(private fb: FormBuilder, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     const data = this.route.snapshot.data;
-    console.log(data['userData']);
-    this.student = data['userData'];
+    this.student = data['userData'].user;
+    this.studentStats = data['userData'].userStats;
+    this.top10 = data['userData'].top10;
+    this.top10.sort((a, b) => a.rank - b.rank);
     this.profileForm = this.fb.group({
       name: [this.student.firstName],
       email: [{ value: this.student.username, disabled: true }]
     });
   }
 
-  onFileSelect(event: any) {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (e: any) => this.profilePicture = e.target.result;
-      reader.readAsDataURL(file);
-    }
-  }
-
   saveProfile() {
     console.log('Saving:', {
       ...this.student,
       name: this.profileForm.value.name,
-      profilePicture: this.profilePicture
     });
     // TODO: Call your API here
   }
