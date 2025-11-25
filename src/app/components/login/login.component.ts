8bf9f7f8b39c1e8fabef89b218ad43b423fd0f7b
Erdenedalai <edalai0115@gmail.com>
2025-11-25 22:05:19 +0800

    [fix] added eye icon

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -14,6 +14,7 @@ export class LoginComponent {
   remember: Boolean = false;
   showForgotPassword: boolean = false;
   resetEmail: string = '';
+  showPassword: boolean = false;
 
   constructor(
     private router: Router,
