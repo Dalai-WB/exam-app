f7d7f7c7a0a1cf4112d68a2931ae7bcf852515eb
Erdenedalai <edalai0115@gmail.com>
2026-02-08 14:01:37 +0800

    fix

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -37,7 +37,7 @@ export class CardComponent {
       this.msg.add({
         severity: 'warn',
         summary: 'Анхааруулга',
-        detail: 'Өмнөх сорилго өгсний дараа дараагийн сорилго хийнэ үү!',
+        detail: 'Та сорилгоо дарааллын дагуу хийнэ үү!',
       });
       return
     }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://exam-app-backend-9f57.onrender.com/',
-  apiUrl: 'http://localhost:3000/',
+  // apiUrl: 'http://localhost:3000/',
+  apiUrl: 'https://e-sorilgo.mn/api/',
   firebase: {
     apiKey: "AIzaSyAL_4cAqVn-FZNenQLI0R3OWEs8jiuz090",
     authDomain: "exap-app-ddc39.firebaseapp.com",
